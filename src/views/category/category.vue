f6170819cf57aaef88f0647b474eb95b062393b0
wxy-133 <w15279621208@163.com>
2021-05-17 23:31:29 +0800

    '05-17滚动区域bug解决'

--- src/views/category/category.vue
+++ src/views/category/category.vue
@@ -1,19 +1,145 @@
 <!--  -->
 <template>
-  <div class="category">
+  <div class="wrapper category" ref="aaa">
+  <ul class="content">
     <h2>分类</h2>
+   <li>1111</li>
+   <li>1112</li>
+   <li>1113</li>
+   <li>1114</li>
+   <li>1115</li>
+   <li>1116</li>
+   <li>1117</li>
+   <li>1118</li>
+   <li>1119</li>
+   <li>11110</li>
+   <li>11111</li>
+   <li>11112</li>
+   <li>11113</li>
+   <li>11114</li>
+   <li>11115</li>
+   <li>11116</li>
+   <li>11117</li>
+   <li>11118</li>
+   <li>11119</li>
+   <li>11120</li>
+   <li>11121</li>
+   <li>11122</li>
+   <li>11123</li>
+   <li>11124</li>
+   <li>11125</li>
+   <li>11126</li>
+   <li>11127</li>
+   <li>11128</li>
+   <li>11129</li>
+   <li>11130</li>
+   <li>11131</li>
+   <li>11132</li>
+   <li>11133</li>
+   <li>11134</li>
+   <li>11135</li>
+   <li>11136</li>
+   <li>11137</li>
+   <li>11138</li>
+   <li>11139</li>
+   <li>11140</li>
+   <li>11141</li>
+   <li>11142</li>
+   <li>11143</li>
+   <li>11144</li>
+   <li>11145</li>
+   <li>11146</li>
+   <li>11147</li>
+   <li>11148</li>
+   <li>11149</li>
+   <li>11150</li>
+   <li>11151</li>
+   <li>11152</li>
+   <li>11153</li>
+   <li>11154</li>
+   <li>11155</li>
+   <li>11156</li>
+   <li>11157</li>
+   <li>11158</li>
+   <li>11159</li>
+   <li>11160</li>
+   <li>11161</li>
+   <li>11162</li>
+   <li>11163</li>
+   <li>11164</li>
+   <li>11165</li>
+   <li>11166</li>
+   <li>11167</li>
+   <li>11168</li>
+   <li>11169</li>
+   <li>11170</li>
+   <li>11171</li>
+   <li>11172</li>
+   <li>11173</li>
+   <li>11174</li>
+   <li>11175</li>
+   <li>11176</li>
+   <li>11177</li>
+   <li>11178</li>
+   <li>11179</li>
+   <li>11180</li>
+   <li>11181</li>
+   <li>11182</li>
+   <li>11183</li>
+   <li>11184</li>
+   <li>11185</li>
+   <li>11186</li>
+   <li>11187</li>
+   <li>11188</li>
+   <li>11189</li>
+   <li>11190</li>
+   <li>11191</li>
+   <li>11192</li>
+   <li>11193</li>
+   <li>11194</li>
+   <li>11195</li>
+   <li>11196</li>
+   <li>11197</li>
+   <li>11198</li>
+   <li>11199</li>
+   <li>111100</li>
+  </ul>
   </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
   name: "category",
   components: {},
   data() {
-    return {};
+    return {
+      scroll:null
+    };
   },
-  computed: {},
+  mounted(){
+    this.scroll= new BScroll(this.$refs.aaa,{
+      probeType:2,
+      click:true,
+      pullUpLoad:true
+    })
+    // this.scroll.on('scroll',(position)=>{
+    //   console.log(position)
+    // })
+     this.scroll.on('pullingUp',()=>{
+      console.log('pull')
+    })
+  },
+  components:{
+    BScroll
+  }
 };
 </script>
 <style  scoped>
+.wrapper{
+  width: 100%;
+  height: 500px;
+  background: red;
+  overflow: hidden;
+}
 </style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -4,13 +4,16 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
-    <homeSwiper :banners="banners" />
-    <homeRecommend :recommends="recommends" />
-    <homeFeatureView />
-    <tabBarControl class="tabbar-control" 
-                  :titles="['流行','新款','精选']" 
-                  @tabClick="tabClick"/>
-    <GoodsLists :goods="goods[currentType].list"/>
+    <Scroll class="content" ref="scorll" 
+    :probe-type="3"
+     @scroll="contentScroll"   >
+      <homeSwiper :banners="banners" />
+      <homeRecommend :recommends="recommends" />
+      <homeFeatureView />
+      <tabBarControl class="tabbar-control" :titles="['流行','新款','精选']" @tabClick="tabClick" />
+      <GoodsLists :goods="goods[currentType].list" />
+    </Scroll>
+    <BackTop @click.native="backTop" v-show="isBackTopShow" />
   </div>
 </template>
 
@@ -18,8 +21,10 @@
 import Swiper from "@/components/common/swiper/Swiper";
 import SwiperItem from "@/components/common/swiper/SwiperItem";
 import NavBar from "@/components/common/navbar/NavBar";
+import Scroll from "@/components/common/scroll/Scroll";
 import tabBarControl from "@/components/content/tabBarControl/tabBarControl";
 import GoodsLists from "@/components/content/goods/GoodsList";
+import BackTop from "@/components/content/backTop/backTop";
 import homeSwiper from "./childComps/homeSwiper";
 import homeRecommend from "./childComps/homeRecommend";
 import homeFeatureView from "./childComps/homeFeatureView";
@@ -32,11 +37,12 @@ export default {
       banners: [],
       recommends: [],
       goods: {
-        'pop': {page: 0, list: [] },
-        'new': {page: 0, list: [] },
-        'sell': {page: 0, list: [] }
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] }
       },
-      currentType:'pop'
+      currentType: "pop",
+      isBackTopShow: false
     };
   },
   computed: {},
@@ -44,38 +50,50 @@ export default {
     NavBar,
     Swiper,
     SwiperItem,
+    Scroll,
     tabBarControl,
     homeSwiper,
     homeRecommend,
     homeFeatureView,
-    GoodsLists
+    GoodsLists,
+    BackTop
   },
   created() {
     //请求轮播，推荐数据
     this.getHomeMultidata();
     //请求商品数据
-    this.getHomeGoods('pop');
-    this.getHomeGoods('new');
-    this.getHomeGoods('sell');
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
   },
   methods: {
     //事件监听相关的方法
-    tabClick(index){
-       switch(index){
-         case 0:{
-           this.currentType='pop'
-           break
-         }
-         case 1:{
-           this.currentType='new'
-           break
-         }
-         case 2:{
-           this.currentType='sell'
-           break
-         }
-       }
+    tabClick(index) {
+      switch (index) {
+        case 0: {
+          this.currentType = "pop";
+          break;
+        }
+        case 1: {
+          this.currentType = "new";
+          break;
+        }
+        case 2: {
+          this.currentType = "sell";
+          break;
+        }
+      }
     },
+    backTop() {
+      this.$refs.scorll.scrollTo(0, 0, 500);
+    },
+    contentScroll(position) {
+      this.isBackTopShow = -position.y > 1000;
+    },
+     loadMore() {
+        this.getHomeGoods(this.currentType)
+        console.log("上拉了");
+      },
     //网络请求相关的方法
     getHomeMultidata() {
       getHomeMultidata().then(res => {
@@ -85,19 +103,23 @@ export default {
       });
     },
     getHomeGoods(type) {
-      const page=this.goods[type].page+1;
+      const page = this.goods[type].page + 1;
       getHomeGoods(type, page).then(res => {
         this.goods[type].list.push(...res.data.list);
-        console.log(res.data)
-        this.goods[type].page+=1;
-      });
+        console.log(res.data);
+        this.goods[type].page += 1;
+        // this.$refs.scorll.finishPullUp()
+      })
     }
-  }
+  },
+  mounted() {}
 };
 </script>
 <style  scoped>
 .home {
   padding-top: 44px;
+  height: 100vh;
+  position: relative;
 }
 .home-nav {
   background-color: var(--color-tint);
@@ -116,4 +138,14 @@ export default {
   top: 44px;
   z-index: 9;
 }
+.content {
+  position: absolute;
+  overflow: hidden;
+  top: 44px;
+  bottom: 49px;
+}
+/*.content{
+  height: calc(100vh - 93px);
+  overflow: hidden;
+}*/
 </style>
